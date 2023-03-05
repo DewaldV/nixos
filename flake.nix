0f2478a1d59fbfda875d36192c2e4fe28d091f2f
DewaldV <dewald.viljoen@pm.me>
2023-03-05 19:58:30 +0000

    flake: update deps

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixos-hardware.url =
       "github:nixos/nixos-hardware?rev=77de4cd09db4dbee9551ed2853cfcf113d7dc5ce";
 
-    # ipu6-nix.url = "path:../ipu6-nix";
+    ipu6-nix.url = "github:dewaldv/ipu6-nix";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
@@ -16,19 +16,21 @@
     };
   };
 
-  outputs =
-    { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager, ...
+    }@inputs:
     let
 
       mkHost = import ./lib/mkhost.nix;
     in {
-      nixosConfigurations.dv-rvu-x1c10 = mkHost "dv-rvu-x1c10" {
+      nixosConfigurations.dv-rvu-x1c10 = mkHost "dv-rvu-x1c10" rec {
         inherit nixpkgs home-manager;
         system = "x86_64-linux";
         user = "dewaldv";
         hardware = nixos-hardware.nixosModules.lenovo-thinkpad-x1-10th-gen;
         overlays = [
+          inputs.ipu6-nix.overlay.${system}
           (final: prev: {
+
             steampipe =
               nixpkgs-unstable.legacyPackages.${prev.system}.steampipe;
           })
