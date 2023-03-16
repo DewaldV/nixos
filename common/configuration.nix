ebb17338fd6f6e51851064606ba93047730ed64a
DewaldV <dewald.viljoen@pm.me>
2023-03-16 14:49:43 +0000

    kernel: update to 6.2

--- common/configuration.nix
+++ common/configuration.nix
@@ -5,7 +5,7 @@
   nixpkgs.config.allowUnfree = true;
 
   # Boot
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  boot.kernelPackages = pkgs.linuxPackages_6_2;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   boot.plymouth.enable = true;
