3a6cbe9388040c2e32ef801afa46e094db21704a
DewaldV <dewald.viljoen@pm.me>
2023-03-21 22:17:53 +0000

    flake: add rvu nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
 
     ipu6-nix.url = "github:dewaldv/ipu6-nix";
     nixpkgs-dewaldv.url = "github:dewaldv/nixpkgs";
+    nixpkgs-rvu.url =
+      "github:uswitch/nixpkgs?ref=2bae351ca4d104960f5a69719054bd8f149002cb";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
@@ -28,16 +30,12 @@
 
         extraModules = [
           inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-10th-gen
-          inputs.nixpkgs-dewaldv.nixosModules.rvu-kolide
+          inputs.nixpkgs-rvu.nixosModule
         ];
 
         overlays = [
           inputs.ipu6-nix.overlay.${system}
           inputs.nixpkgs-dewaldv.overlay.${system}
-          (final: prev: {
-            steampipe =
-              inputs.nixpkgs-unstable.legacyPackages.${prev.system}.steampipe;
-          })
         ];
       };
 
