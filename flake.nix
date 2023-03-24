f43b0ab72cb92fe946a560073a5a9adc2562a663
DewaldV <dewald.viljoen@pm.me>
2023-03-24 19:41:31 +0000

    flake: formatting

--- flake.nix
+++ flake.nix
@@ -3,14 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-dewaldv.url = "github:dewaldv/nixpkgs";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-rvu.url = "github:uswitch/nixpkgs?ref=kolide";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
     ipu6-nix.url = "github:dewaldv/ipu6-nix";
-    nixpkgs-dewaldv.url = "github:dewaldv/nixpkgs";
-    nixpkgs-rvu.url =
-      "github:uswitch/nixpkgs?ref=2bae351ca4d104960f5a69719054bd8f149002cb";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
@@ -19,9 +17,7 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-
-      mkHost = import ./lib/mkhost.nix;
+    let mkHost = import ./lib/mkhost.nix;
     in {
       nixosConfigurations.dv-rvu-x1c10 = mkHost "dv-rvu-x1c10" rec {
         inherit nixpkgs home-manager;
