823eb98cad2dcaf8dbbeb21267f985f59a7d1f50
DewaldV <dewald.viljoen@rvu.co.uk>
2023-05-08 15:35:19 +0100

    sway: layout and battery icons

--- users/dewaldv/home-manager.nix
+++ users/dewaldv/home-manager.nix
@@ -31,7 +31,14 @@
         statusCommand = null;
       }];
 
-      input = { "*" = { xkb_layout = "gb"; }; };
+      input = {
+        "*" = {
+          xkb_layout = "gb";
+          xkb_options = "ctrl:nocaps";
+        };
+      };
+
+      output = { "eDP-1" = { scale = "1.5"; }; };
     };
   };
 
@@ -51,7 +58,7 @@
         modules-left = [ "sway/workspaces" "sway/mode" ];
         modules-center = [ "sway/window" ];
         modules-right =
-          [ "pulseaudio" "network" "cpu" "memory" "tray" "clock" ];
+          [ "pulseaudio" "network" "cpu" "memory" "battery" "tray" "clock" ];
         "sway/workspaces" = {
           disable-scroll = true;
           all-outputs = false;
@@ -73,30 +80,30 @@
           spacing = 10;
         };
         clock = { format-alt = "{:%Y-%m-%d}"; };
-        cpu = { format = "{usage}% "; };
-        memory = { format = "{}% "; };
-        # "battery": {
-        #     "bat": "BAT0",
-        #     "states": {
-        #         // "good": 95,
-        #         "warning": 30,
-        #         "critical": 15
-        #     },
-        #     "format": "{capacity}% {icon}",
-        #     // "format-good": "", // An empty format will hide the module
-        #     // "format-full": "",
-        #     "format-icons": ["", "", "", "", ""]
-        # },
+        cpu = { format = " {load}"; };
+        memory = { format = " {}%"; };
+        battery = {
+          bat = "BAT0";
+          states = {
+            good = 95;
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon}";
+          # "format-good" = ""; // An empty format will hide the module
+          # "format-full" = "";
+          format-icons = [ "" "" "" "" "" ];
+        };
         network = {
           # "interface": "wlp2s0", // (Optional) To force the use of this interface
-          format-wifi = "{essid} ({signalStrength}%) ";
-          format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
-          format-disconnected = "Disconnected ⚠";
+          format-wifi = " {essid} ({signalStrength}%)";
+          format-ethernet = " {ifname}: {ipaddr}/{cidr}";
+          format-disconnected = "⚠ Disconnected";
         };
         pulseaudio = {
           # "scroll-step": 1,
-          format = "{volume}% {icon}";
-          format-bluetooth = "{volume}% {icon}";
+          format = "{icon} {volume}%";
+          format-bluetooth = "{icon}  {volume}% ";
           format-muted = "";
           format-icons = {
             headphones = "";
