3ce8d48f3d1a8762224ba7ced1ca238759a40c9a
DewaldV <dewald.viljoen@pm.me>
2023-07-06 18:20:30 +0100

    dv-desktop: remove deprecated hardware setting

--- machines/dv-desktop/hardware.nix
+++ machines/dv-desktop/hardware.nix
@@ -39,8 +39,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 
   hardware.bluetooth.enable = true;
 
