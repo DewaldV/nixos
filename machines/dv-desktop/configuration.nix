8d1ddd52b3a32046343459db04cdf4a790d3f859
DewaldV <dewald.viljoen@pm.me>
2023-08-20 16:16:36 +0100

    dv-desktop: enable gamemode

--- machines/dv-desktop/configuration.nix
+++ machines/dv-desktop/configuration.nix
@@ -10,6 +10,11 @@
   services.xserver.layout = "us";
   services.blueman.enable = true;
 
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+  };
+
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
