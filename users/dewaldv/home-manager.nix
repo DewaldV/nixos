ebea0b1aa34bfbf0d4d49ba91fca077ef4820e4a
DewaldV <dewald.viljoen@pm.me>
2023-10-06 14:05:05 +0100

    gtk: configure dark mode

--- users/dewaldv/home-manager.nix
+++ users/dewaldv/home-manager.nix
@@ -10,6 +10,12 @@ machineSettings:
     size = 16;
   };
 
+  gtk = {
+    enable = true;
+    gtk3.extraConfig = { "gtk-application-prefer-dark-theme" = 1; };
+    gtk4.extraConfig = { "gtk-application-prefer-dark-theme" = 1; };
+  };
+
   wayland.windowManager.sway = {
     enable = true;
     config = rec {
