4d3332a8e221f3f02c3f882e5230c35816e5df27
DewaldV <dewald.viljoen@pm.me>
2023-11-16 14:43:44 +0000

    sway: reindent config

--- machines/dv-desktop/home/sway.nix
+++ machines/dv-desktop/home/sway.nix
@@ -1,16 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  wayland.windowManager.sway = {
-    config = { input = lib.mkForce { "*" = { xkb_layout = "us"; }; }; };
+  wayland.windowManager.sway.config = {
+    input = lib.mkForce { "*" = { xkb_layout = "us"; }; };
   };
 
-  programs.waybar = {
-    settings = {
-      mainBar = {
-        temperature = {
-          hwmon-path = lib.mkForce "/sys/class/hwmon/hwmon3/temp1_input";
-        };
+  programs.waybar.settings = {
+    mainBar = {
+      temperature = {
+        hwmon-path = lib.mkForce "/sys/class/hwmon/hwmon3/temp1_input";
       };
     };
   };
