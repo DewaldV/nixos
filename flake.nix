7274928733c870af545826cd9455becacee16df9
DewaldV <dewald.viljoen@pm.me>
2023-12-02 22:30:00 +0000

    nixos: upgrade to 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS System";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
