d08d3665fd01b374a1be154d0c0b1d50a457aa48
DewaldV <dewald.viljoen@pm.me>
2023-12-21 19:44:49 +0000

    podman: clean up setting

--- common/configuration.nix
+++ common/configuration.nix
@@ -217,7 +217,7 @@
 
   security.pam.services.swaylock = { };
 
-  virtualisation = { podman = { enable = true; }; };
+  virtualisation.podman.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
