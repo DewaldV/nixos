a55c17666305ad86a2567686f2f9e7a6925cd0de
DewaldV <dewald.viljoen@pm.me>
2023-12-29 19:38:46 +0200

    haxe: add more openfl deps

--- machines/dv-fw/user.nix
+++ machines/dv-fw/user.nix
@@ -1,5 +1,14 @@
 { config, pkgs, home-manager, ... }:
 
 {
-  users.users.dewaldv.packages = with pkgs; [ godot_4 haxe ];
+  users.users.dewaldv.packages = with pkgs; [
+    godot_4
+    haxe
+    libX11
+    libXext
+    libXi
+    libXinerama
+    libxrandr
+    neko
+  ];
 }
