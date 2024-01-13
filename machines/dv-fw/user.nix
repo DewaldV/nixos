58a9d7b37b93cbdae9b1122d5312a6012f351935
DewaldV <dewald.viljoen@pm.me>
2024-01-13 13:17:13 +0200

    haxe: remove haxe temporarily

--- machines/dv-fw/user.nix
+++ machines/dv-fw/user.nix
@@ -1,14 +1,5 @@
 { config, pkgs, home-manager, ... }:
 
 {
-  users.users.dewaldv.packages = with pkgs; [
-    godot_4
-    haxe
-    libX11
-    libXext
-    libXi
-    libXinerama
-    libxrandr
-    neko
-  ];
+  users.users.dewaldv.packages = with pkgs; [ godot_4 ];
 }
