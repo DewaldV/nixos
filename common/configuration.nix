1ae1ee54867286beea1af7f7ee1680aae94e14e8
DewaldV <dewald.viljoen@pm.me>
2024-01-26 18:05:37 +0000

    kernel: upgrade to 6.7 series kernel

--- common/configuration.nix
+++ common/configuration.nix
@@ -8,7 +8,7 @@
   # Boot
   boot = {
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_6_6;
+    kernelPackages = pkgs.linuxPackages_6_7;
     kernelParams = [ "quiet" ];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = true;
