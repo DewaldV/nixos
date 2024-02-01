e216f3bba5229002383b0663b452745bfed769fd
DewaldV <dewald.viljoen@pm.me>
2024-02-01 21:08:52 +0000

    Enable vagrant and virtualbox

--- machines/dv-desktop/virt.nix
+++ machines/dv-desktop/virt.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  environment.systemPackages = with pkgs; [ vagrant ];
+  virtualisation.virtualbox.host.enable = true;
 
-  users.users.dewaldv = { extraGroups = [ "libvirtd" ]; };
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  users.users.dewaldv = { extraGroups = [ "libvirtd" "vboxusers" "docker" ]; };
 }
