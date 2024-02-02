5fee185f8e2e8cee5ad2e10d3d1418a928618168
DewaldV <dewald.viljoen@pm.me>
2024-02-02 17:37:14 +0000

    sway: configurre xdg-desktop-portal

--- common/configuration.nix
+++ common/configuration.nix
@@ -201,17 +201,11 @@
     # gtk portal needed to make gtk apps happy
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-    # warning: xdg-desktop-portal 1.17 reworked how portal implementations are loaded, you
-    # should either set `xdg.portal.config` or `xdg.portal.configPackages`
-    # to specify which portal backend to use for the requested interface.
-
-    # https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in
-
-    # If you simply want to keep the behaviour in < 1.17, which uses the first
-    # portal implementation found in lexicographical order, use the following:
-
-    # xdg.portal.config.common.default = "*";
-    config.common.default = "*";
+    config.common = {
+      default = "gtk";
+      "org.freedesktop.impl.portal.Screencast" = "wlr";
+      "org.freedesktop.impl.portal.Screenshot" = "wlr";
+    };
   };
 
   security.pam.services.swaylock = { };
