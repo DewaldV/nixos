ce77d2602187127913dff7a3ece8a3f4d3389671
DewaldV <dewald.viljoen@pm.me>
2024-03-03 16:12:30 +0000

    sway: theme swaylock

--- home/sway.nix
+++ home/sway.nix
@@ -174,41 +174,46 @@
   };
 
   programs.swaylock = {
+    package = pkgs.swaylock-effects;
     settings = {
       show-failed-attempts = true;
-      font = "Roboto";
+      font = "Noto Sans, Light";
+      font-size = 32;
+      clock = true;
+      timestr = "%H:%M";
+      datestr = "%a %d %b %Y";
+      indicator = true;
 
-      color = "1D1D1D";
-      indicator-radius = "240";
-      indicator-thickness = "20";
-
-      key-hl-color = "880033";
-
-      inside-color = "00000099";
-      inside-clear-color = "ffd20400";
-      inside-caps-lock-color = "009ddc00";
-      inside-ver-color = "d9d8d800";
-      inside-wrong-color = "ee2e2400";
-
-      ring-color = "231f20D9";
-      ring-clear-color = "231f20D9";
-      ring-caps-lock-color = "231f20D9";
-      ring-ver-color = "231f20D9";
-      ring-wrong-color = "231f20D9";
-
+      # catppuccin/swaylock: https://github.com/catppuccin/swaylock/blob/main/themes/mocha.conf
+      color = "1e1e2e";
+      bs-hl-color = "f5e0dc";
+      caps-lock-bs-hl-color = "f5e0dc";
+      caps-lock-key-hl-color = "a6e3a1";
+      inside-color = "00000000";
+      inside-clear-color = "00000000";
+      inside-caps-lock-color = "00000000";
+      inside-ver-color = "00000000";
+      inside-wrong-color = "00000000";
+      key-hl-color = "a6e3a1";
+      layout-bg-color = "00000000";
+      layout-border-color = "00000000";
+      layout-text-color = "cdd6f4";
       line-color = "00000000";
-      line-clear-color = "ffd204FF";
-      line-caps-lock-color = "009ddcFF";
-      line-ver-color = "d9d8d8FF";
-      line-wrong-color = "ee2e24FF";
-
-      text-clear-color = "ffd20400";
-      text-ver-color = "d9d8d800";
-      text-wrong-color = "ee2e2400";
-
-      bs-hl-color = "ee2e24FF";
-      caps-lock-key-hl-color = "ffd204FF";
-      caps-lock-bs-hl-color = "ee2e24FF";
+      line-clear-color = "00000000";
+      line-caps-lock-color = "00000000";
+      line-ver-color = "00000000";
+      line-wrong-color = "00000000";
+      ring-color = "b4befe";
+      ring-clear-color = "f5e0dc";
+      ring-caps-lock-color = "fab387";
+      ring-ver-color = "89b4fa";
+      ring-wrong-color = "eba0ac";
+      separator-color = "00000000";
+      text-color = "cdd6f4";
+      text-clear-color = "f5e0dc";
+      text-caps-lock-color = "fab387";
+      text-ver-color = "89b4fa";
+      text-wrong-color = "eba0ac";
     };
   };
 
