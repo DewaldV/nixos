9748f4d1fee3e073e4fa730d60e7be20ac2a57f3
DewaldV <dewald.viljoen@pm.me>
2024-03-17 10:05:55 +0000

    dv-desktop: restructure amdvlk section

--- machines/dv-desktop/hardware.nix
+++ machines/dv-desktop/hardware.nix
@@ -62,20 +62,21 @@
 
   hardware.bluetooth.enable = true;
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    #   package = pkgs-unstable.mesa.drivers;
-    #   package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
-    #   extraPackages = [ pkgs-unstable.amdvlk ];
-    #   extraPackages32 = [ pkgs-unstable.driversi686Linux.amdvlk ];
-  };
-
   # amdvlk seems to have lower performance than mesa radv
   # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/amd/default.nix
   # https://nixos.org/manual/nixos/unstable/index.html#sec-gpu-accel-vulkan
   # https://nixos.wiki/wiki/AMD_GPU
   # https://www.phoronix.com/review/amdvlk-radv-rx7900
-  hardware.amdgpu.amdvlk = false;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+
+    # package = pkgs-unstable.mesa.drivers;
+    # package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
+    # extraPackages = [ pkgs-unstable.amdvlk ];
+    # extraPackages32 = [ pkgs-unstable.driversi686Linux.amdvlk ];
+  };
+
+  hardware.amdgpu.amdvlk = true;
 }
