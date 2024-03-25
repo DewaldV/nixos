86777cde0d99b75ae7195bf44a61d09f063a98e2
DewaldV <dewald.viljoen@rvu.co.uk>
2024-03-25 17:17:15 +0000

    rvu-x1c10: configure RSA key for tempcover

--- machines/dv-rvu-x1c10/home/git.nix
+++ machines/dv-rvu-x1c10/home/git.nix
@@ -3,19 +3,34 @@ let
   email = "dewald.viljoen@rvu.co.uk";
   sshPubKey =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYV7aiA/Iu1NJWA/i3NB/eazTJafGSqrm7LCPaIzwQ8";
+  sshRSAPubKey =
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjuu4vEU3rcsKa11Ro5+w+Zrl3flqIQ03ZsfRDD9+FV6rQ2X9NnaAgclYmFSQ3lCJrw7vpiQ76FK2W8tCX6P7m62Qg2PKMFFHqXfmY4G8muJGFgSWB1lCcJpY7naTIkXILs0wjdzKr5VHf8nUsQUrvGKhd+VgXUeLKubgxjKLonk7BjObpedle16ONY8NGQJwL0doWXNDVcQq+gtRrQHw4YW6YwUMy+wGsAD+lKw3wr2/9BcmB9lVS5CO+LXUgjfbsfoox4r/KMkI1CJzzRMnwJUFKToCpTfkC2Grq9XZ89PEJOwYylsDzMFIVXE1cpm5/xcND6PyE5mgFRI+V7DQ3";
 in {
   programs.git = {
     userEmail = lib.mkForce email;
     signing.key = lib.mkForce sshPubKey;
+    includes = [{
+      condition = "gitdir:~/Projects/tempcover";
+      contents = { user = { signingKey = sshRSAPubKey; }; };
+    }];
   };
 
   programs.ssh = {
     matchBlocks = {
       "github.com" = { identityFile = lib.mkForce "~/.ssh/rvu"; };
+      "ssh.dev.azure.com" = {
+        hostname = "ssh.dev.azure.com";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/rvu-rsa";
+        extraOptions = { IdentityAgent = "~/.1password/agent.sock"; };
+      };
     };
   };
 
   home.file.".ssh/rvu".text = sshPubKey;
-  home.file.".ssh/allowed_signers".text =
-    lib.mkForce ''${email} namespaces="git" ${sshPubKey}'';
+  home.file.".ssh/rvu-rsa".text = sshRSAPubKey;
+  home.file.".ssh/allowed_signers".text = lib.mkForce ''
+    ${email} namespaces="git" ${sshPubKey}
+    ${email} namespaces="git" ${sshRSAPubKey}
+  '';
 }
