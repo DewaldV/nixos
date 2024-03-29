85621224261a0e273e563ca651ab8272d7b69961
DewaldV <dewald.viljoen@rvu.co.uk>
2024-03-29 21:18:32 +0000

    doom: search projects and dev recursively for projects

--- home/doom/doom.d/config.el
+++ home/doom/doom.d/config.el
@@ -30,7 +30,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documents/notes/")
-(setq projectile-project-search-path '(("~/Projects/" . 1) "~/Dev/"))
+(setq projectile-project-search-path '(("~/Projects/" . 2) ("~/Dev/" . 2)))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
