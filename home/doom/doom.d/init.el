7cf0b74cfa9a1ba29b099614aea9b1e75a763fa6
DewaldV <dewald.viljoen@rvu.co.uk>
2024-03-29 18:43:42 +0000

    doom: enable csharp and clojure language support

--- home/doom/doom.d/init.el
+++ home/doom/doom.d/init.el
@@ -112,11 +112,11 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;cc                ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
+       clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
