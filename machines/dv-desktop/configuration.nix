828a502aa402be466ffe16fd2c338eba69ed9c49
DewaldV <dewald.viljoen@pm.me>
2024-04-21 14:14:26 +0100

    steam: enable gamescope

--- machines/dv-desktop/configuration.nix
+++ machines/dv-desktop/configuration.nix
@@ -15,7 +15,10 @@
     enableRenice = true;
   };
 
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     lutris
