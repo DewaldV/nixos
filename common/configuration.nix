8dfe478adb91a278f9fe99df9d82a5ff3d7b4a05
DewaldV <dewald.viljoen@pm.me>
2024-07-28 14:11:48 +0100

    deps: update deps and kernel to 6.10

--- common/configuration.nix
+++ common/configuration.nix
@@ -16,7 +16,7 @@
   # Boot
   boot = {
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_6_9;
+    kernelPackages = pkgs.linuxPackages_6_10;
     kernelParams = [ "quiet" ];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = true;
