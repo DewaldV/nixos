1d64c8bc17a44dd0474579bef1ec16167eafd547
DewaldV <dewald.viljoen@pm.me>
2024-10-12 21:15:51 +0100

    waybar: update styling

--- sway/waybar.nix
+++ sway/waybar.nix
@@ -13,10 +13,11 @@
         fonts = [
           "FontAwesome"
           "Noto Sans"
+          "Hack"
         ];
         layer = "top"; # Waybar at top layer
         position = "top"; # Waybar at the top of your screen
-        height = 24; # Waybar Height
+        height = 36; # Waybar Height
         # "width": 1366, // Waybar width
         # Choose the order of the modules
         modules-left = [
@@ -27,8 +28,8 @@
         modules-right = [
           "pulseaudio"
           "network"
-          "cpu"
           "temperature"
+          "cpu"
           "memory"
           "battery"
           "tray"
@@ -61,10 +62,20 @@
         };
         temperature = {
           format = " {temperatureC}°C";
-          hwmon-path = "/sys/class/hwmon/hwmon1/temp1_input";
+          hwmon-path = "/sys/class/hwmon/hwmon5/temp1_input";
         };
         cpu = {
-          format = " {load}";
+          format = "{icon} {load}";
+          format-icons = [
+            "<span color='#69ff94'>▁</span>"
+            "<span color='#2aa9ff'>▂</span>"
+            "<span color='#f8f8f2'>▃</span>"
+            "<span color='#f8f8f2'>▄</span>"
+            "<span color='#ffffa5'>▅</span>"
+            "<span color='#ffffa5'>▆</span>"
+            "<span color='#ff9977'>▇</span>"
+            "<span color='#dd532e'>█</span>"
+          ];
         };
         memory = {
           format = " {}%";
@@ -76,7 +87,7 @@
             warning = 25;
             critical = 15;
           };
-          format = "{capacity}% {icon}";
+          format = "{icon} {capacity}%";
           # "format-good" = ""; // An empty format will hide the module
           # "format-full" = "";
           format-icons = [
@@ -123,153 +134,102 @@
     };
 
     style = ''
-      /*
-      *
-      * Catppuccin Mocha palette
-      * Maintainer: rubyowo
-      *
-      */
-
-      @define-color base   #1e1e2e;
-      @define-color mantle #181825;
-      @define-color crust  #11111b;
-
-      @define-color text     #cdd6f4;
-      @define-color subtext0 #a6adc8;
-      @define-color subtext1 #bac2de;
-
-      @define-color surface0 #313244;
-      @define-color surface1 #45475a;
-      @define-color surface2 #585b70;
-
-      @define-color overlay0 #6c7086;
-      @define-color overlay1 #7f849c;
-      @define-color overlay2 #9399b2;
-
-      @define-color blue      #89b4fa;
-      @define-color lavender  #b4befe;
-      @define-color sapphire  #74c7ec;
-      @define-color sky       #89dceb;
-      @define-color teal      #94e2d5;
-      @define-color green     #a6e3a1;
-      @define-color yellow    #f9e2af;
-      @define-color peach     #fab387;
-      @define-color maroon    #eba0ac;
-      @define-color red       #f38ba8;
-      @define-color mauve     #cba6f7;
-      @define-color pink      #f5c2e7;
-      @define-color flamingo  #f2cdcd;
-      @define-color rosewater #f5e0dc;
+      @define-color base   #1a1a1a;
+      @define-color text   #d1d1d1;
 
       * {
-          border: none;
-          border-radius: 4px;
-          font-family: FontAwesome, Noto Sans, Roboto, Helvetica, Arial, sans-serif;
-          font-size: 14px;
-          min-height: 0;
+        border: none;
+        font-family: Noto Sans, FontAwesome;
+        font-size: 16px;
+        min-height: 0;
+        color: @text;
       }
 
       window#waybar {
-          background: @base;
+        background: @base;
       }
 
       #window {
-          background: @surface0;
-          color: @lavender;
-          padding: 0 10px;
-          font-weight: normal;
-          font-family: "Hack";
+        padding: 0 10px;
+        font-weight: normal;
       }
 
-      /* #workspaces { */
-      /*     background: @base; */
-      /*     padding: 0 5px; */
-      /* } */
-
       #workspaces button {
-          padding: 0 10px;
-          background: @surface0;
-          color: @blue;
-          border-top: 2px solid transparent;
+        padding: 0 10px;
+        border-top: 2px solid transparent;
       }
 
       #workspaces button.focused {
-          color: @green;
-          border-top: 2px solid @green;
+        border-top: 2px solid;
       }
 
       .modules-left {
-          background: @surface0;
-          border-right: 0px solid;
-          border-left: 0px;
+        border-right: 0px solid;
+        border-left: 0px;
       }
 
       .modules-right {
-          background: @surface0;
-          border-left: 0px solid;
-          border-right: 0px;
-          padding-left: 5px;
+        border-left: 0px solid;
+        border-right: 0px;
+        padding-left: 5px;
       }
 
       #clock, #battery, #cpu, #memory, #network, #pulseaudio, #custom-spotify, #tray {
-          background: @surface0;
-          padding: 0 3px;
-          margin: 0 2px;
+        padding: 0 3px;
+        margin: 0 2px;
+      }
+
+      #cpu, #temperature, #memory, #battery {
+        font-family: Hack;
+        border-left: 0px solid;
+        border-radius: 0;
+        margin-top: 4px;
+        margin-bottom: 4px;
+        padding-left: 4px;
       }
 
       #mode {
-          color: @peach;
-          background: @surface0;
-          border-top: 2px solid @peach;
-          padding: 0 3px;
-          margin: 0 2px;
+        border-top: 2px solid;
+        padding: 0 3px;
+        margin: 0 2px;
       }
 
       #clock {
-          color: @subtext1;
-          font-weight: bold;
+        font-weight: bold;
       }
 
       #battery {
-          color: @peach
       }
 
       #battery icon {
-          color: @yellow;
       }
 
       #battery.charging {
       }
 
       #memory {
-          color: @green;
       }
 
       #cpu {
-          color: @teal;
       }
 
       #temperature {
-          color: @teal;
       }
 
       #network {
-          color: @sky;
       }
 
       #network.disconnected {
-          border-top: 2px solid @sky;
+        border-top: 2px solid;
       }
 
       #pulseaudio {
-          color: @sapphire;
       }
 
       #pulseaudio.muted {
       }
 
       #tray {
-          background: @surface0
       }
     '';
   };
