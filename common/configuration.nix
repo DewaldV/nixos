c7ef88e358e1a50cc0be1ac9d262b093346ec772
DewaldV <dewald.viljoen@pm.me>
2024-10-30 21:50:58 +0000

    kernel: update to 6.11 release

--- common/configuration.nix
+++ common/configuration.nix
@@ -16,7 +16,7 @@
   # Boot
   boot = {
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_6_10;
+    kernelPackages = pkgs.linuxPackages_6_11;
     kernelParams = [ "quiet" ];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = true;
