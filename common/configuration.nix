c9aa65ac716ed13fe2a3e314bfcd3c4caedf2694
DewaldV <dewald.viljoen@pm.me>
2025-06-20 11:54:01 +0100

    kernel: upgrade to 6.15 release

--- common/configuration.nix
+++ common/configuration.nix
@@ -16,7 +16,7 @@
   # Boot
   boot = {
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_6_14;
+    kernelPackages = pkgs.linuxPackages_6_15;
     kernelParams = [ "quiet" ];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = true;
