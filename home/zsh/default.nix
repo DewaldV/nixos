a7d1173399fa56a9a0d257273381d594b9c1d086
DewaldV <dewald.viljoen@pm.me>
2025-07-04 17:14:01 +0100

    asdf-vm: provide custom init

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -8,7 +8,6 @@
       theme = "robbyrussell";
       custom = "\${HOME}/.zsh-custom";
       plugins = [
-        "asdf"
         "aws"
         "azure"
         "emacs"
@@ -23,6 +22,14 @@
     };
   };
 
+  home.file.".zsh-custom/asdf.zsh".text = ''
+    export ASDF_DIR=${pkgs.asdf-vm}
+    . ${pkgs.asdf-vm}/share/asdf-vm/asdf.sh
+    # append completions to fpath
+    fpath=(''${ASDF_DIR}/completions $fpath)
+    # initialise completions with ZSH's compinit
+    autoload -Uz compinit && compinit
+  '';
   home.file.".zsh-custom/bin.zsh".source = ./zsh-custom/bin.zsh;
   home.file.".zsh-custom/go.zsh".source = ./zsh-custom/go.zsh;
   home.file.".zsh-custom/kube.zsh".source = ./zsh-custom/kube.zsh;
