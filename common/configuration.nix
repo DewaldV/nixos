0ef1bcd6bc29afccf642d9dd99c4757f8abae4b3
DewaldV <dewald.viljoen@pm.me>
2025-10-27 17:26:22 +0000

    kernel: upgrade to 6.17 release

--- common/configuration.nix
+++ common/configuration.nix
@@ -16,7 +16,7 @@
   # Boot
   boot = {
     initrd.systemd.enable = true;
-    kernelPackages = pkgs.linuxPackages_6_16;
+    kernelPackages = pkgs.linuxPackages_6_17;
     kernelParams = [ "quiet" ];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = true;
