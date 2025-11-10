7bc375bb07368e306f2a33badd2c55c18e49d0ed
DewaldV <dewald.viljoen@pm.me>
2025-11-10 09:05:40 +0000

    zsh: remove custom asdf init

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -24,14 +24,6 @@
     };
   };
 
-  # home.file.".zsh-custom/asdf.zsh".text = ''
-  #   export ASDF_DIR=${pkgs.asdf-vm}
-  #   . ${pkgs.asdf-vm}/share/asdf-vm/asdf.sh
-  #   # append completions to fpath
-  #   fpath=(''${ASDF_DIR}/completions $fpath)
-  #   # initialise completions with ZSH's compinit
-  #   autoload -Uz compinit && compinit
-  # '';
   home.file.".zsh-custom/bin.zsh".source = ./zsh-custom/bin.zsh;
   home.file.".zsh-custom/go.zsh".source = ./zsh-custom/go.zsh;
   home.file.".zsh-custom/kube.zsh".source = ./zsh-custom/kube.zsh;
