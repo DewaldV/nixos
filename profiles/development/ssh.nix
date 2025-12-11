03a7d982765652bc58e7a657b565e2d26e47510b
DewaldV <dewald.viljoen@pm.me>
2025-12-11 21:09:37 +0000

    ssh: use personal key for general ssh

--- profiles/development/ssh.nix
+++ profiles/development/ssh.nix
@@ -18,6 +18,11 @@
         controlMaster = "no";
         controlPath = "~/.ssh/master-%r@%n:%p";
         controlPersist = "no";
+        identityFile = "~/.ssh/personal-ed25519";
+        identitiesOnly = true;
+        extraOptions = {
+          IdentityAgent = "~/.1password/agent.sock";
+        };
       };
 
       "github.com" = {
