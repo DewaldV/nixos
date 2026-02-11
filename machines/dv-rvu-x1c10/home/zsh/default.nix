ab2b68f6513b6831d82cfc556b5281b11bf822a7
DewaldV <dewald.viljoen@rvu.co.uk>
2026-02-11 10:45:31 +0000

    rvu: export litellm key into shell

--- machines/dv-rvu-x1c10/home/zsh/default.nix
+++ machines/dv-rvu-x1c10/home/zsh/default.nix
@@ -3,4 +3,7 @@
 {
   home.file.".zsh-custom/rust.zsh".source = ./zsh-custom/rust.zsh;
   home.file.".zsh-custom/rvu.zsh".source = ./zsh-custom/rvu.zsh;
+  home.file.".zsh-custom/litellm.zsh".text = ''
+    export LITE_LLM_API_KEY=$(cat ${config.age.secrets."rvu-litellm-personal-key".path});
+  '';
 }
