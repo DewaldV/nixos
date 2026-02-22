03b3a1bae0fffc26a0f6eb659669f59e736be45e
DewaldV <dv@dewaldv.com>
2026-02-22 22:00:31 +0000

    tmux: add minimal-tmux-status with tomorrow-night colours

--- profiles/shell/tmux.nix
+++ profiles/shell/tmux.nix
@@ -17,6 +17,21 @@
       tmuxPlugins.sensible
       tmuxPlugins.fzf-tmux-url
       {
+        plugin = tmuxPlugins.minimal-tmux-status;
+        extraConfig = ''
+          set -g @minimal-tmux-fg "#1d1f21"
+          set -g @minimal-tmux-bg "#81a2be"
+          set -g @minimal-tmux-justify "left"
+          set -g @minimal-tmux-indicator-str " tmux "
+          set -g @minimal-tmux-status "bottom"
+          set -g @minimal-tmux-right true
+          set -g @minimal-tmux-left true
+          set -g @minimal-tmux-use-arrow true
+          set -g @minimal-tmux-right-arrow ""
+          set -g @minimal-tmux-left-arrow ""
+        '';
+      }
+      {
         plugin = tmuxPlugins.vim-tmux-navigator;
         extraConfig = ''
           set -g @vim_navigator_pattern '(\S+/)?g?\.?(view|l?n?vim?x?|fzf|\.?emacs[^/]*)(diff)?(-wrapped)?'
