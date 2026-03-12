6b795c4b3dd90c49f760eaab638a4b4e727f481b
DewaldV <dv@dewaldv.com>
2026-03-12 21:34:17 +0000

    gtk: fix dark mode for modern apps

--- profiles/base/gtk.nix
+++ profiles/base/gtk.nix
@@ -8,11 +8,26 @@
 {
   gtk = {
     enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
     gtk3.extraConfig = {
       "gtk-application-prefer-dark-theme" = 1;
     };
-    gtk4.extraConfig = {
-      "gtk-application-prefer-dark-theme" = 1;
+  };
+
+  # GTK4/libadwaita apps read color-scheme from dconf rather than gtk settings
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
     };
   };
 }
