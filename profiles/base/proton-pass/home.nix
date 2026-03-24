c2b3f4eb5cf196993877e16b07dce144c015e287
DewaldV <dewald.viljoen@rvu.co.uk>
2026-03-24 10:35:20 +0000

    proton-pass-cli: bump to 1.8.0 directly until upstream available

--- profiles/base/proton-pass/home.nix
+++ profiles/base/proton-pass/home.nix
@@ -5,10 +5,29 @@
   ...
 }:
 
+let
+  # TODO: remove once 1.8.0 reaches nixpkgs unstable
+  sources = {
+    "x86_64-linux" = pkgs-unstable.fetchurl {
+      url = "https://proton.me/download/pass-cli/1.8.0/pass-cli-linux-x86_64";
+      hash = "sha256-M7zWxVYHHjM86/l3K+0AR8QceiydP0n0sXj9rSctaeI=";
+    };
+    "aarch64-linux" = pkgs-unstable.fetchurl {
+      url = "https://proton.me/download/pass-cli/1.8.0/pass-cli-linux-aarch64";
+      hash = "sha256-t3AyOLF0mXh9eCxBOh3e8WPBPowv9pWZ2WQTCTl+StA=";
+    };
+  };
+  # TODO: remove once 1.8.0 reaches nixpkgs unstable
+  proton-pass-cli = pkgs-unstable.proton-pass-cli.overrideAttrs (old: {
+    version = "1.8.0";
+    src = sources.${pkgs-unstable.stdenv.hostPlatform.system};
+  });
+in
+
 {
   home.packages = [
     pkgs-unstable.proton-pass
-    pkgs-unstable.proton-pass-cli
+    proton-pass-cli
   ];
 
   # Use the D-Bus Secret Service (GNOME Keyring) as the keyring backend so
