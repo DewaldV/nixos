44f248f5664c862f9b83eb9e4a180431b9b9ed26
DewaldV <dv@dewaldv.com>
2026-04-08 20:45:53 +0100

    jellyfin: grant permissions for va-api

--- profiles/jellyfin/configuration.nix
+++ profiles/jellyfin/configuration.nix
@@ -10,4 +10,10 @@
   services.jellyfin = {
     enable = true;
   };
+
+  # Grant jellyfin access to the GPU for VA-API hardware transcoding
+  users.users.jellyfin.extraGroups = [
+    "render" # access to /dev/dri/renderD128
+    "video" # access to /dev/dri/card0
+  ];
 }
