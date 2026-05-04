8741e58185afd73c3ae9626ea3216f09c8e9a2ee
DewaldV <dv@dewaldv.com>
2026-05-04 08:58:58 +0100

    dl-vm: import qemu-guest profile for modules

--- machines/dl-vm/default.nix
+++ machines/dl-vm/default.nix
@@ -1,9 +1,15 @@
-{ disko, ... }:
+{ modulesPath, disko, ... }:
 
 {
   imports = [
+    # Hardware-specific modules
     disko.nixosModules.disko
+    (modulesPath + "/profiles/qemu-guest.nix")
+
+    # Profiles
     ../../profiles/sshd
+
+    # Machine-specific config
     ./configuration.nix
     ./disko.nix
     ./hardware.nix
--- machines/dl-vm/hardware.nix
+++ machines/dl-vm/hardware.nix
@@ -1,17 +1,5 @@
 { lib, ... }:
 
 {
-  boot.initrd.availableKernelModules = [
-    "virtio_pci"
-    "virtio_scsi"
-    "virtio_blk"
-    "virtio_net"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
