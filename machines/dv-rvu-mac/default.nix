35a815997e09222cbd9f03290b2612a12f089c5e
DewaldV <dewald.viljoen@rvu.co.uk>
2026-07-20 19:47:41 +0100

    dv-rvu-mac: add coreutils and fontconfig

--- machines/dv-rvu-mac/default.nix
+++ machines/dv-rvu-mac/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ ../../profiles/nix/darwin.nix ];
@@ -7,4 +7,18 @@
   system.stateVersion = 6;
 
   programs.zsh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    coreutils-prefixed
+    fontconfig
+  ];
+
+  fonts = {
+    packages = with pkgs; [
+      hack-font
+      emacs-all-the-icons-fonts
+      nerd-fonts.hack
+      # symbola # currently unavailable
+    ];
+  };
 }
